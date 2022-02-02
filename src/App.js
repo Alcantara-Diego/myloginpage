202a5678d5dc08c222740a10dd4ab1fc1d125e65
Diego Alcântara <ddag0309@gmail.com>
2022-02-02 20:01:53 -0300

    teste com HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './styles/style.css';
 import Home from './components/Home';
 import Login from './components/Login';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
@@ -10,10 +10,10 @@ function App() {
 
       <Router>
         <Routes>
-          <Route path="/" element={ <Home/>}></Route>
+          <Route path="/" element={ <Home/>}/>
 
 
-          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </Router>
      
